e224afb3a5eb2db88b2da4ebb345c36f63116e01
Rashad Majali <rashad612@yahoo.com>
2011-04-02 19:29:40 +0300

    content length and markup

--- ibm_translate.php
+++ ibm_translate.php
@@ -2,9 +2,11 @@
   function translate($srvc_id, $text) {
     $ch = curl_init();
     $text = drupal_urlencode($text);
-    
-    curl_setopt($ch, CURLOPT_URL, 'http://198.145.243.38/axis/tr.jsp?svcid='.$srvc_id.'&text='.$text);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($text)));
+    //print $text; die();
+    curl_setopt($ch, CURLOPT_URL, 'http://198.145.243.38/axis/tr.jsp?svcid=' . $srvc_id.'&text=' . $text);
+    //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($text)));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: 0'));
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $body = curl_exec($ch);
     curl_close($ch);
